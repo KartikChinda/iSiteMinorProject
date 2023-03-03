d7761e5622bb5340787954746593cc932b395cd7
KartikChinda <2005665@kiit.ac.in>
2023-03-03 16:48:34 +0530

    Hiding passwords for API keys

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import userRoutes from './Routes/users.js'
+import * as dotenv from 'dotenv';
+dotenv.config()
+console.log(process.env)
 
 
 const app = express();
@@ -16,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-const CONN_URL = 'mongodb+srv://kartikChinda:iSiteMinorProject@cluster0.zxwsqrn.mongodb.net/?retryWrites=true&w=majority'
+const CONN_URL = `mongodb+srv://kartikChinda:${process.env.PASS}@cluster0.zxwsqrn.mongodb.net/?retryWrites=true&w=majority`;
 
 const PORT = process.env.PORT || 5000
 
