678b9ad9e46c58f8cd15e5aff5e1de6eae95029e
KartikChinda <2005665@kiit.ac.in>
2023-03-03 16:40:32 +0530

    The ring gets forged

---
+++ backend/Controllers/users.js
@@ -0,0 +1,3 @@
+export const getUsers = (req, res) => {
+    res.send("All users will be shown here.");
+}
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+// MongoPass: iSiteMinorProject
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import userRoutes from './Routes/users.js'
+
+
+const app = express();
+
+app.use('/users', userRoutes);
+
+app.use(bodyParser.json({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+const CONN_URL = 'mongodb+srv://kartikChinda:iSiteMinorProject@cluster0.zxwsqrn.mongodb.net/?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONN_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server up and running on Port ${PORT}`)))
+    .catch((error) => console.log(error))
+
+
